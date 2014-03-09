224aac621939c2f9c7f7812feb0583f049006ca6
0x14 <0x14@users.noreply.github.com>
2014-03-09 18:45:03 +0000

    ex1.py
    ex1.py

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print 'How do you take your coffee?'
+print 'How many tsps sugar?'
+sugar=int(raw_input())
+print 'How many tsps coffee?'
+coffee=int(raw_input())
+print 'Heres your coffee, %d tsps sugar, %d tsps coffee.' %(sugar, coffee)
+if coffee>1: print 'Thats a strong coffee!'
