25dd2ab9f27865295ddfb22b2ee051e2c2a85918
0x14 <0x14@users.noreply.github.com>
2014-03-09 19:05:05 +0000

    This was from memory
    So the code is pretty wrong around the milk question. I will next paste the fixed version, you could call it debugging (lol not yet) where i used the interpreter to tell me where i was wrong and adjusted accordingly (all from memory yay!)

--- ex1.py
+++ ex1.py
@@ -1,7 +1,26 @@
-print 'How do you take your coffee?'
+from time import sleep
+
+print 'Im brewing up. How do you take your coffee?'
+sleep(2)
+
+print 'How much milk? Input 1 for a little, 2 for lots.'
+milkinput = raw_input()
+
+if milkinput = '1'
+  then milk = 'a little'
+  
+else milkinput = '2'
+  then milk = 'lots of'
+
+sleep(2)
+
 print 'How many tsps sugar?'
 sugar=int(raw_input())
+sleep(2)
+
 print 'How many tsps coffee?'
 coffee=int(raw_input())
-print 'Heres your coffee, %d tsps sugar, %d tsps coffee.' %(sugar, coffee)
+sleep(2)
+
+print 'Heres your coffee, with %s milk, %d tsps sugar, %d tsps coffee.' %(milk, sugar, coffee)
 if coffee>1: print 'Thats a strong coffee!'
